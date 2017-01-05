320546ee7a007f9cf3460210c93c9a3f1921bab5
ShivanKaul <shivankaul.1993@gmail.com>
2017-01-05 10:48:46 +0530

    Add dimensions

--- typewriter.py
+++ typewriter.py
@@ -9,11 +9,14 @@ import scipy.misc
 
 
 def generate_image(input_file, output_file,
+                   height=700, width=700,
                    poet_name="bug",
                    font_name="KingthingsTrypewriter2", font_size=24):
-
-    poem = 'echo "$(cat ' + input_file + ')\n\n-' + poet_name + '"'
-    imageMagick = ('convert -fill black -font ' + font_name +
+    # Append poet name to poem
+    poem = 'echo "$(cat ' + input_file + ')\n\n...\n\n-' + poet_name + '"'
+    # Do image magic
+    dimensions = str(height) + 'x' + str(width)
+    imageMagick = ('convert -size ' + dimensions + ' -font ' + font_name +
                    ' -pointsize ' + str(font_size) +
                    ' -gravity center label:@- ' +
                    output_file)
@@ -21,7 +24,13 @@ def generate_image(input_file, output_file,
     os.system(cmd)
 
 
+def make_vignette(input_file, output_file):
+    cmd = "convert " + input_file + " -background black -vignette 50x50  " + output_file
+    os.system(cmd)
+
+
 def create_typewriter_effect(img):
+    # Post processing
     gradient = np.gradient(img)
     print(gradient)
     new_img = np.zeros(img.shape)
@@ -44,8 +53,8 @@ def create_typewriter_effect(img):
 
 
 def generate_gaussian(sigma, N):
-    neg = -int(N/2)
-    pos = int(N/2)
+    neg = -int(N / 2)
+    pos = int(N / 2)
 
     x_vals = range(neg, pos)
     y_vals = range(neg, pos)
@@ -56,8 +65,13 @@ def generate_gaussian(sigma, N):
     g = np.divide(g, np.sum(g))
     return g
 
+if len(sys.argv) < 2:
+    print("Please provide path to poem text")
+    sys.exit()
 input_file = sys.argv[1]
 output_file = "poem_" + input_file + ".gif"
 generate_image(input_file, output_file)
-img = scipy.misc.imread(output_file) / 255
-create_typewriter_effect(img)
+# make_vignette(output_file, output_file)
+# Post processing
+# img = scipy.misc.imread(output_file) / 255
+# create_typewriter_effect(img)
